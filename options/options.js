d69f92090f466ffd7f11d23866ac73ddfe2a5693
Crowdbotics <team@crowdbotics.com>
2021-08-11 18:07:54 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "chatlata_29726Identifier",
+  url: "https://chatlata_29726Identifier.botics.co",
+  api: "https://chatlata_29726Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
